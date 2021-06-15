52a34c786d97f4d2a8663b2755130f12b8d8233d
akswosn <akswosn@gmail.com>
2021-06-15 12:36:27 +0900

    2021-06-15 프로젝트 생성

---
+++ README.md
@@ -0,0 +1,2 @@
+# Token 검증 데모 프로젝트
+spring-boot 2.5.1
---
+++ src/test/java/com/akswosn/tokenapi/TokenApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akswosn.tokenapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TokenApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
